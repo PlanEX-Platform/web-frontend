edbeae366f492217775c66b48a4c412db3db0aa3
Alexey Zhunin <zhunin.a.u@gmail.com>
2018-03-22 22:32:41 +0300

    drafts of Home page components

---
+++ src/components/BuyOrder.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="box">
+    <p class="title">Buy</p>
+  </div>
+</template>
---
+++ src/components/InfoTips.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="box">
+    <p class="title">Tips</p>
+  </div>
+</template>
