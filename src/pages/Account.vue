918805222b15d006c0716856aea2cdf26fe009d7
Andrey Zolin <crrrr@mail.ru>
2018-03-22 23:52:01 +0300

    top and navigation

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,3 +1,57 @@
 <template>
-  <div>Account page</div>
+  <div>
+    <top></top>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-half">
+            <h1 class="title">Account settings</h1>
+            <b-field>
+              <strong>Email address: </strong>{{email}}
+            </b-field>
+
+            <b-field label="Old Password">
+              <b-input type="password"
+                       placeholder="Old Password"
+                       password-reveal>
+              </b-input>
+            </b-field>
+
+            <b-field label="Password">
+              <b-input type="password"
+                       placeholder="New Password"
+                       password-reveal>
+              </b-input>
+            </b-field>
+            <b-field label="Password">
+              <b-input type="password"
+                       placeholder="Repeat Password"
+                       password-reveal>
+              </b-input>
+            </b-field>
+            <b-field>
+              <a class="button is-primary">Save</a>
+            </b-field>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
+
+<script>
+import Top from '@/components/Top.vue'
+import BField from "buefy/src/components/field/Field";
+
+export default {
+  components: {
+    BField,
+    Top
+  },
+  data () {
+    return {
+      email: 'some@some.com'
+    }
+  }
+}
+</script>
--- src/pages/Balance.vue
+++ src/pages/Balance.vue
@@ -1,41 +1,49 @@
 <template>
-  <b-table
-    :data="data"
-    paginated
-    per-page="5"
-    :opened-detailed="defaultOpenedDetails"
-    detailed
-    narrowed
-    detail-key="coin"
-  >
+  <div>
+    <top></top>
+    <section class="section">
+      <div class="container">
+        <b-table
+          :data="data"
+          paginated
+          per-page="5"
+          :opened-detailed="defaultOpenedDetails"
+          detailed
+          narrowed
+          detail-key="coin"
+        >
 
-    <template slot-scope="props">
+          <template slot-scope="props">
 
-      <b-table-column field="coin" label="Coin" sortable>
-        {{ props.row.coin }}
-      </b-table-column>
+            <b-table-column field="coin" label="Coin" sortable>
+              {{ props.row.coin }}
+            </b-table-column>
 
-      <b-table-column field="name" label="Name" sortable>
-        {{ props.row.name }}
-      </b-table-column>
+            <b-table-column field="name" label="Name" sortable>
+              {{ props.row.name }}
+            </b-table-column>
 
-      <b-table-column field="balance" label="Balance" sortable centered>
-        {{ props.row.balance }}
-      </b-table-column>
+            <b-table-column field="balance" label="Balance" sortable centered>
+              {{ props.row.balance }}
+            </b-table-column>
 
-      <b-table-column field="inOrder" label="In Order" sortable centered>
-        {{ props.row.inOrder }}
-      </b-table-column>
-    </template>
+            <b-table-column field="inOrder" label="In Order" sortable centered>
+              {{ props.row.inOrder }}
+            </b-table-column>
+          </template>
 
-    <template slot="detail" slot-scope="props">
-      <a class="button">Deposit</a>
-      <a class="button">Withdraw</a>
-    </template>
-  </b-table>
+          <template slot="detail" slot-scope="props">
+            <a class="button" href="/deposit">Deposit</a>
+            <a class="button" href="/withdraw">Withdraw</a>
+          </template>
+        </b-table>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import Top from '@/components/Top.vue'
 
 const data = [
   {'coin': 'ETH', 'name': 'Ethereum', 'balance': 123, 'inOrder': 1},
@@ -43,6 +51,9 @@ const data = [
 ]
 
 export default {
+  components: {
+    Top
+  },
   data () {
     return {
       data,
